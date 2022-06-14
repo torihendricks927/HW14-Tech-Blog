51d82e60ebb37d246d09854ca61ed0707936d3e3
Tori <torihendricks927@yahoo.com>
2022-06-14 10:09:17 -0500

    adding login js, page, and route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,52 +2,52 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const projectData = await Project.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
 //     // Serialize data so the template can read it
 //     // const projects = projectData.map((project) => project.get({ plain: true }));
 
 
-//     const blogs = [
-//       {
-//         title: "title",
-//         content: "content",
-//         author: "author",
-//         date: "6/14/22"
-//       },
-//       {
-//         title: "title",
-//         content: "content",
-//         author: "author",
-//         date: "6/14/22"
-//       },
-//       {
-//         title: "title",
-//         content: "content",
-//         author: "author",
-//         date: "6/14/22"
-//       },
-//     ];
+    const blogs = [
+      {
+        title: "title",
+        content: "content",
+        author: "author",
+        date: "6/14/22"
+      },
+      {
+        title: "title",
+        content: "content",
+        author: "author",
+        date: "6/14/22"
+      },
+      {
+        title: "title",
+        content: "content",
+        author: "author",
+        date: "6/14/22"
+      },
+    ];
 
 //     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       blogs, 
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('homepage', { 
+      blogs, 
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/project/:id', async (req, res) => {
   try {
