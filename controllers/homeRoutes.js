80c18233163b92a52eed2f2ec302ef9f55bf6f8e
Tori <torihendricks927@yahoo.com>
2022-06-14 09:57:58 -0500

    testing without database due to issue with server

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,52 +2,52 @@ const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const projectData = await Project.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
 
-    // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
+//     // Serialize data so the template can read it
+//     // const projects = projectData.map((project) => project.get({ plain: true }));
 
 
-    const blogs = [
-      {
-        title: "title",
-        content: "content",
-        author: "author",
-        date: "6/14/22"
-      },
-      {
-        title: "title",
-        content: "content",
-        author: "author",
-        date: "6/14/22"
-      },
-      {
-        title: "title",
-        content: "content",
-        author: "author",
-        date: "6/14/22"
-      },
-    ];
+//     const blogs = [
+//       {
+//         title: "title",
+//         content: "content",
+//         author: "author",
+//         date: "6/14/22"
+//       },
+//       {
+//         title: "title",
+//         content: "content",
+//         author: "author",
+//         date: "6/14/22"
+//       },
+//       {
+//         title: "title",
+//         content: "content",
+//         author: "author",
+//         date: "6/14/22"
+//       },
+//     ];
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      blogs, 
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     // Pass serialized data and session flag into template
+//     res.render('homepage', { 
+//       blogs, 
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/project/:id', async (req, res) => {
   try {
@@ -72,24 +72,24 @@ router.get('/project/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
 
-//     const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+//const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
+//const sequelize = require('./config/connection');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -36,8 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
 
 app.listen(PORT, () => console.log('Listening'));
